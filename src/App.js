8ef4de158f049b1bedfe85f699d9fa30dfb41dcc
AkhmalNovanda <cocwar427@gmail.com>
2023-02-02 16:22:47 +0700

    Movie App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useEffect, useState } from "react"
+import SearchIcon from './assets/loupe.png'
+import HeaderIcon from './assets/Movie.png'
+import './app.css'
+import Card from "./MovieCard"
+import Load from "./load"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL = 'http://www.omdbapi.com?apikey=90557500'
+const App = () => {
+
+
+   const [movies, setMovies] = useState([])
+   const [seacrhTerm, setSearchTerm] = useState('')
+
+
+
+   const seacrhMovie = async (title) => {
+      const responseAPi = await fetch(`${API_URL}&s=${title}`)
+      const data = await responseAPi.json()
+
+      setMovies(data.Search)
+      console.log(data)
+   }
+
+   useEffect(() => {
+      seacrhMovie('Batman')
+   }, [])
+
+   const hanldeOnChange = (event) => {
+      setSearchTerm(event.target.value)
+   }
+   const hanldeOnSubmit = () => {
+      seacrhMovie(seacrhTerm)
+   }
+
+
+   return (
+      <div className="App">
+         <div className="header">
+            <div className="header-title" style={{display : 'flex'}}>
+               <img src={HeaderIcon} alt = {HeaderIcon} style = {{width : '80px', height : '70px'}}/>
+               <h1>MovieApp</h1>
+            </div>
+            <div className="header-input">
+               <input
+                  placeholder="Search for Movies"
+                  value= {seacrhTerm}
+                  onChange={hanldeOnChange}
+               />
+               <img
+                  src={SearchIcon}
+                  alt="Search"
+                  onClick={hanldeOnSubmit}
+               />
+            </div>
+         </div>
+         <div className="searched">
+            <h2>You are Searching For "{seacrhTerm}"</h2>
+         </div>
+         {movies?.length > 0 ? (
+            <div className="container">
+               {movies.map((movie, idx) => {
+                  return(
+                     <Card key={idx} movie={ movie }/>
+                  )
+               })}
+            </div>
+         ) :
+         (
+            <Load/>
+         ) 
+         }
+
+
+
+      </div>
+   )
 }
 
-export default App;
+
+export default App
---
+++ src/load.jsx
@@ -0,0 +1,20 @@
+
+
+
+
+const Load = () => {
+   return (
+      <div className="loading-container">
+         <div class="sk-chase">
+         <div class="sk-chase-dot"></div>
+         <div class="sk-chase-dot"></div>
+         <div class="sk-chase-dot"></div>
+         <div class="sk-chase-dot"></div>
+         <div class="sk-chase-dot"></div>
+         <div class="sk-chase-dot"></div>
+      </div>
+      </div>
+   )
+}
+
+export default Load 
